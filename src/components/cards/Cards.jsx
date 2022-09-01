3b73389d9ff4338ddcdc9d4117496a8953fc7e84
Pedro Gabriel <pedrogabriellima@gmail.com>
2022-09-01 13:07:10 -0300

    adding figure and figcaption

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -8,25 +8,31 @@ function Card(){
         <>
             <div id="card">
                 <div className='card-elements'>
-                    <img src={LiftersShop} 
-                    alt="Projeto Lifters Shop." />
-                    <h2>Lifters Shop</h2>
+                    <figure>
+                        <img src={LiftersShop} 
+                        alt="Projeto Lifters Shop." />
+                        <figcaption>Lifters Shop</figcaption>
+                    </figure>
                     <span className='card-link'>
                         <a href='https://liftershop.herokuapp.com/' target='_blank' rel='noreferrer'>Visit here.</a>
                     </span>
                 </div>
                 <div className='card-elements'>
-                    <img src={GovPB}
-                    alt="Projeto Governo da Paraíba." />
-                    <h2>Register GovPB</h2>
+                    <figure>
+                        <img src={GovPB}
+                        alt="Projeto Governo da Paraíba." />
+                        <figcaption>Register GovPB</figcaption>
+                    </figure>
                     <span className='card-link'>
                         <a href='https://registergovpb.herokuapp.com/' target='_blank' rel='noreferrer'>Visit here.</a>
                     </span>
                 </div>
                 <div className='card-elements'>
-                    <img src={LinkTree}
-                    alt="Projeto Arvore de Links." />
-                    <h2>LinkTree</h2>
+                    <figure>
+                        <img src={LinkTree}
+                        alt="Projeto Arvore de Links." />
+                        <figcaption>LinkTree</figcaption>
+                    </figure>
                     <span className='card-link'>
                         <a href="https://pedrogles.github.io/LinkTree/" target='_blank' rel='noreferrer'>Visit here.</a>
                     </span>
