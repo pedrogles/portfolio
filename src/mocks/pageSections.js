74a64d324e3cba7d22f284a7b452dc71be51d65b
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-07 16:38:39 -0300

    Feat: add sections data for navigation menu

---
+++ src/mocks/pageSections.js
@@ -0,0 +1,17 @@
+export const sections = [
+    {
+        id: 1,
+        name: 'Início',
+        path: '/'
+    },
+    {
+        id: 2,
+        name: 'Sobre mim',
+        path: '/sobre'
+    },
+    {
+        id: 3,
+        name: 'Projetos',
+        path: '/projetos'
+    },
+];
