3838c2a25e461246d3fcda5368e7c1edded08f5a
Pedro Gabriel Lima e Silva <76228682+pedrogles@users.noreply.github.com>
2025-06-10 17:50:11 -0300

    Remove old lowercase aboutPage folder to avoid duplication with renamed AboutPage

--- src/pages/aboutPage/components/index.js
+++
@@ -1,6 +0,0 @@
-export { AboutMe } from './AboutMe/AboutMe';
-export { Educations } from './Educations/Education';
-export { Experiences } from './Experiences/Experiences';
-export { ListItem } from './ListItem/ListItem';
-export { Skills } from './Skills/Skills';
-export { ProfileCard } from './ProfileCard/ProfileCard';
